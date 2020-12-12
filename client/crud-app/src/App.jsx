9e8ed01cebfb0d23ea35d801960f893b2258de17
bhara <bharathJ222000@gmail.com>
2020-12-12 18:08:28 +0530

    added

--- client/crud-app/src/App.jsx
+++ client/crud-app/src/App.jsx
@@ -14,11 +14,12 @@ class App extends React.Component {
     }
 
     createData = (data) => {
-        if (!data.isEdit) {
+        if(!data.isEdit) {
             axios.post("http://localhost:5100/info", data).then(res => {
                 this.getData();
             })
-        } else {
+        }
+        else {
             axios.put("http://localhost:5100/info/edit", data).then(res => {
                 this.getData();
             })
@@ -42,39 +43,30 @@ class App extends React.Component {
         this.setState({
             editData: data
         })
-    }
+}
 
     delete = (data) => {
         var option = window.confirm(`Are You Sure You Want To Delete ${data.Name}`);
-        if (option) {
+        if(option) {
             axios.delete(`http://localhost:5100/info/delete/${data._id}`).then(res => {
-                console.log(res.data);
-                this.getData();
-            })
+            console.log(res.data);
+            this.getData();
+        })
         }
     }
 
     render() {
-        return ( <
-            div className = "container mt-4" >
-            <
-            div className = "row" >
-            <
-            div className = "col-6" >
-            <
-            Form myData = { this.createData }
-            setForm = { this.state.editData }
-            /> <
-            /div> <
-            div className = "col-6" >
-            <
-            Table getData = { this.state.data }
-            setData = { this.update }
-            delData = { this.delete }
-            /> <
-            /div> <
-            /div> <
-            /div>
+        return(
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="col-6">
+                        <Form myData = {this.createData} setForm = {this.state.editData} />
+                    </div>
+                    <div className="col-6">
+                        <Table getData = {this.state.data} setData = {this.update} delData = {this.delete} />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
---
+++ client/crud-app/src/components/Table.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Table extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>City</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.getData.length > 0 ? 
+                        (
+                            this.props.getData.map(data => 
+                                <tr key = {data._id}>
+                                    <td>{data.Name}</td>
+                                    <td>{data.Age}</td>
+                                    <td>{data.City}</td>
+                                    <td><button className="btn btn-success" 
+                                        onClick={(event) => {
+                                            this.props.setData(data)
+                                        }} 
+                                    >Edit</button></td>
+                                    <td><button className="btn btn-danger"
+                                        onClick={(event) => {
+                                            this.props.delData(data)
+                                        }} 
+                                    >Delete</button></td>
+                                </tr>
+                            )
+                        )
+                        :
+                        (
+                            <tr>
+                                <td>No Data</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Table;
